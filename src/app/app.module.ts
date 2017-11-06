232230ccf34d460d29738d5308327b10c10b4f4f
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-06 08:30:32 -0800

    created a gallery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ng2-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
