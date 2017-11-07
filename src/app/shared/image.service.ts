a761174e8a37265b8d7011f48d34374a32f0d40a
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-07 08:01:25 -0800

    added more objects to the service to apply pipe

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -27,12 +27,48 @@ const IMAGES: any[] = [
       "id": 2,
       "category": "boat",
       "caption": "a view from a boat",
-      "url": "https://d1elspe5sgezyc.cloudfront.net/assets/v3/home/owners/power1-0ad9b4fec1eea01588b77f0948503152.jpg"
+      "url": "http://content.basspro.com/sitelets/boats-atvs/images/brand-tahoe.jpg"
     },
     {
       "id": 3,
       "category": "boat",
       "caption": "a view from a boat",
-      "url": "https://d1elspe5sgezyc.cloudfront.net/assets/v3/home/owners/power1-0ad9b4fec1eea01588b77f0948503152.jpg"
-    }
+      "url": "https://www.envirofloorcare.com/images/boat.jpg"
+    },
+    {
+      "id": 4,
+      "category": "babies",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "https://i.ytimg.com/vi/DSnbZUjIyAc/maxresdefault.jpg"
+    },
+    {
+      "id": 5,
+      "category": "babies",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "http://www.parents.com/sites/parents.com/files/styles/width_300/public/images/p_101395269.jpg"
+    },
+    {
+      "id": 6,
+      "category": "babies",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "http://ww1.hdnux.com/photos/53/14/16/11322252/5/920x920.jpg"
+    },
+    {
+      "id": 7,
+      "category": "pets",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "http://guyanachronicle.com/wp-content/uploads/2016/06/pets.jpg"
+    },
+    {
+      "id": 8,
+      "category": "pets",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "http://www.petmd.com/sites/default/files/classroom-pets3.jpg"
+    },
+    {
+      "id": 9,
+      "category": "pets",
+      "caption": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, debitis iste ab atque asperiores omnis dolorem consequuntur accusamus veritatis ipsam sunt assumenda labore vitae, ipsum perspiciatis dolores laboriosam, non.",
+      "url": "http://images.parents.mdpcdn.com/sites/parents.com/files/images/550_101032377.jpg"
+    },
   ]
