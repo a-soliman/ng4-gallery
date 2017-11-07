a9617f2c084e230bed9fc6cd03e192f040302470
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-07 08:18:22 -0800

    debbuged the service file correcting the category for the boats images

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -19,19 +19,19 @@ export class ImageService {
 const IMAGES: any[] = [
     {
       "id": 1,
-      "category": "boat",
+      "category": "boats",
       "caption": "a view from a boat",
       "url": "https://d1elspe5sgezyc.cloudfront.net/assets/v3/home/owners/power1-0ad9b4fec1eea01588b77f0948503152.jpg"
     },
     {
       "id": 2,
-      "category": "boat",
+      "category": "boats",
       "caption": "a view from a boat",
       "url": "http://content.basspro.com/sitelets/boats-atvs/images/brand-tahoe.jpg"
     },
     {
       "id": 3,
-      "category": "boat",
+      "category": "boats",
       "caption": "a view from a boat",
       "url": "https://www.envirofloorcare.com/images/boat.jpg"
     },
